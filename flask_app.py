1e22c8bbbec28b66a8f33cc74eb733165e08a50d
Dimi Rziankin <dimi.rziankin@gmail.com>
2023-11-21 16:28:23 +0100

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return "<p>Hello world!<p>"
+    return "<p>Hello planet!<p>"
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
